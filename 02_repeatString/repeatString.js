44df31fb47cbe8284bbf6a09fea4fd7de1dae535
Lefasto <g.palfoeldi@gmail.com>
2025-04-07 21:42:03 +0200

    solution 01_helloWorld, 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(input, numberOfRepeats) {
+    let output = "";
+    if(numberOfRepeats < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < numberOfRepeats; i++){
+        output += input;        
+    }
+    return output;
 };
 
 // Do not edit below this line
