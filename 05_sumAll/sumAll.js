1c8cc785e0b70eb678076146f5ebf360c1a0a58c
Lefasto <g.palfoeldi@gmail.com>
2025-04-07 23:23:27 +0200

    solution sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstSummand, secondSummand) {
+    let sum = 0;
+    if(firstSummand > secondSummand){
+        let temp = firstSummand;
+        firstSummand = secondSummand;
+        secondSummand = temp;
+        
+    }
+    if(isNaN(firstSummand) || isNaN(secondSummand)){
+        return "ERROR";
+    }
+    else if(!Number.isInteger(firstSummand) || !Number.isInteger(secondSummand)){
+        return "ERROR";
+    }
+    else if(firstSummand < 0){
+        return "ERROR";
+    }
+    for(let i = firstSummand; i <= secondSummand; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
