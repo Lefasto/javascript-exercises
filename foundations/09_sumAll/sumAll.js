9f932030b694f172c5705d1afba9cce63cf249d7
Gabriel Palfoeldi <g.palfoeldi@gmail.com>
2026-06-08 09:00:25 +0200

    finished sumAll excercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (lower, upper) {
+  let sum = 0;
+  let temp = 0;
+  if (lower > upper) {
+    temp = lower;
+    lower = upper;
+    upper = temp;
+  } else if (lower < 0 || upper < 0) {
+    return "ERROR";
+  } else if (!Number.isInteger(lower) || !Number.isInteger(upper)) {
+    return "ERROR";
+  } else if (typeof lower !== "number" || typeof upper !== "number") {
+    return "ERROR";
+  }
+  for (let i = lower; i <= upper; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
