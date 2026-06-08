fc214d92fc81ae217ad06f3461c55838e56b0401
Gabriel Palfoeldi <g.palfoeldi@gmail.com>
2026-06-08 11:12:32 +0200

    finished tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const celsius = (((fahrenheit - 32) * 5) / 9).toFixed(1);
+  return parseFloat(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const fahrenheit = ((celsius * 9) / 5 + 32).toFixed(1);
+  return parseFloat(fahrenheit);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
