7ba33c21c766a590caa2354906f0d988efc824ac
Gabriel Palfoeldi <g.palfoeldi@gmail.com>
2026-06-10 14:14:00 +0200

    Finished calculator.js

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function (first, second) {
+  return first - second;
 };
 
-const sum = function() {
-	
+const sum = function (numberArray) {
+  let solution = 0;
+
+  for (let i = 0; i < numberArray.length; i++) {
+    solution += numberArray[i];
+  }
+  return solution;
 };
 
-const multiply = function() {
+const multiply = function (numberArray) {
+  let solution = 1;
 
+  for (let i = 0; i < numberArray.length; i++) {
+    solution *= numberArray[i];
+  }
+  return solution;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +42,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
