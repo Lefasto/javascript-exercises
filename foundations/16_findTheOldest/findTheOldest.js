9b63273aae1c016dceae32089384388d32c78ba8
Gabriel Palfoeldi <g.palfoeldi@gmail.com>
2026-06-11 11:21:04 +0200

    finished findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.forEach((person) => {
+    if (person.hasOwnProperty("yearOfDeath")) {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    } else person.age = new Date().getFullYear() - person.yearOfBirth;
+  });
+  return people.reduce((prev, current) =>
+    prev.age > current.age ? prev : current,
+  );
 };
 
 // Do not edit below this line
